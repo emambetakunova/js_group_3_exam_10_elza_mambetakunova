a6493db46e18298ee2b92dd0ca845af9bf86d02f
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-30 18:15:02 +0600

    install bootstrap

--- exam_10_frontend/src/index.js
+++ exam_10_frontend/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import 'react-notifications/lib/notifications.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reducer from './store/reducer/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
