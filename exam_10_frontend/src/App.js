60dd2f8aec987d98257f0b323a958c199295ca0f
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-30 18:14:45 +0600

    add new page

--- exam_10_frontend/src/App.js
+++ exam_10_frontend/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import {Container} from "reactstrap";
+import {Switch, Route} from 'react-router-dom';
+
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import AddNews from "./containers/AddNews/AddNews";
+import News from "./containers/News/News";
+import NewsData from "./components/NewsData/NewsData"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <header><Toolbar/></header>
+                <Container style={{marginTop: '20px', textAlign: "center"}}>
+                    <Switch>
+                        <Route path="/" exact component={News}/>
+                        <Route path="/news" exact component={News}/>
+                        <Route path="/news/new" component={AddNews}/>
+                        <Route path="/news/:id" exact component={NewsData}/>
+                    </Switch>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
